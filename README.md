b11bc6f80f51702e6e6bc532df1c9c1525a08234
Sinan <sinan.sarykaya@gmail.com>
2022-08-07 00:38:24 +0200

    Coppy button added

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # javascript-background-change
+
 Javascript background change app
+
+<a href='https://sinansarikaya.github.io/javascript-background-change/'>Click here for demo </a>
--- main.js
+++ main.js
@@ -4,6 +4,8 @@ const handleChange = () => {
   let colors = document.getElementById("color").value;
   containerColor.style.backgroundColor = colors;
   localStorage.setItem("bgColor", colors);
+  document.getElementById("colorHex").innerHTML =
+    localStorage.getItem("bgColor");
   return colors;
 };
 if (localStorage.getItem("bgColor")) {
@@ -13,3 +15,10 @@ if (localStorage.getItem("bgColor")) {
   containerColor.style.backgroundColor = localStorage.getItem("bgColor");
 }
 document.getElementById("color").value = localStorage.getItem("bgColor");
+document.getElementById("colorHex").innerHTML = localStorage.getItem("bgColor");
+const handleClick = () => {
+  navigator.clipboard.writeText(localStorage.getItem("bgColor"));
+  document.querySelector(
+    ".tooltiptext"
+  ).innerHTML = `Copied: ${localStorage.getItem("bgColor")}`;
+};
